841813551786b9083deeeb4a1d8ff612cf74c295
SUSMIT RUDRA <60251577+Susmit10@users.noreply.github.com>
2020-10-18 09:29:23 +0530

    Version 2

--- app/src/main/java/com/example/headlinehustle/MainActivity.kt
+++ app/src/main/java/com/example/headlinehustle/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.headlinehustle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val items = fetchData()
+        val adapter = NewsListAdapter(items)
+        recyclerView.adapter = adapter
+    }
+
+    private fun fetchData(): ArrayList<String>
+    {
+        val list = ArrayList<String>()
+        for (i in 0 until 100)
+        {
+            list.add("Item $i")
+        }
+        return list
     }
 }
---
+++ app/src/main/java/com/example/headlinehustle/NewsListAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.headlinehustle
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsListAdapter(private val items: ArrayList<String>): RecyclerView.Adapter<NewsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
+        return NewsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val currentItem = items[position]
+        holder.titleView.text = currentItem
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+}
+
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+{
+    val titleView: TextView = itemView.findViewById(R.id.title)
+}
